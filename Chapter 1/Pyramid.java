d900236897e4febc327fc75b0917ceebfca88d21
JOHNREYAMBRAD <johnambrad80@gmail.com>
2024-09-02 08:21:26 +0800

    Pyramid.java

--- Chapter 1/Pyramid.java
+++ Chapter 1/Pyramid.java
@@ -1,25 +1,18 @@
 public class Pyramid{
 
-public static void main (string[] args) {
+public static void main (String[] args) {
 
-int layers = 7;
-
-for (int i=1; i<= Layers; i++) {
-
-for (int j = layers - 1; j > 0; J--) {
-
-System.out.print(": ");
-
-}
-
-for (int k=1; K <= (2 * i-1); k++){
-
-System.out.print("T");
-}
-System.out.println();
-
-}
-
-}
-
+   String[] triangle = {
+   "      T      ",
+   "     TTT     ",
+   "    TTTTT    ",
+   "   TTTTTTT   ",
+   "  TTTTTTTTT  ",
+   " TTTTTTTTTTT ",
+   "TTTTTTTTTTTTT",
+   };
+  for (String line : triangle){
+   System.out.println(line);
+    }
+   }
 }
