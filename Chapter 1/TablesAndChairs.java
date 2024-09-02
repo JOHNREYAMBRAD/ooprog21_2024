f7b7c1553de9d32323a3399aaa530c0639e8a565
JOHNREYAMBRAD <johnambrad80@gmail.com>
2024-09-02 08:21:55 +0800

    TablesAndChairs.java

--- Chapter 1/TablesAndChairs.java
+++ Chapter 1/TablesAndChairs.java
@@ -1,10 +1,10 @@
 public class TableAndChairs {
     public static void main(String[] args){
-        System.out.ptintln("X                   X");
-        System.out.ptintln("X                   X");
-        System.out.ptintln("X      XXXXXXX      X");
-        System.out.ptintln("XXXXXX X     X XXXXXX");
-        System.out.ptintln("X    X X     X X    X");
-        System.out.ptintln("X    X X     X X    X");
+        System.out.println("X                   X");
+        System.out.println("X                   X");
+        System.out.println("X      XXXXXXX      X");
+        System.out.println("XXXXXX X     X XXXXXX");
+        System.out.println("X    X X     X X    X");
+        System.out.println("X    X X     X X    X");
     }
 }
