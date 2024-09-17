406bf5b8272f89b4b44a9ba390e487804578149c
JOHNREYAMBRAD <johnambrad80@gmail.com>
2024-09-18 07:55:49 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FirstAddressExercise{
+   public static void main (String[]args){
+      System.out.println("First java application");
+      displayAddress(); 
+   
+   }
+ public static void displayAddress(){
+  System.out.println("Samsung Electronics");
+ System.out.println("129 Samsung-ro");
+ System.out.println("Yeongtong-gu, Suwon-si");
+ System.out.println("Gyeonggi-do 443-742");
+  System.out.println("company: Samsung Electronics");
+
+
+ 
+ 
+ }
+}
